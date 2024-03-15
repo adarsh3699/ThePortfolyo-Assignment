feb860e95628ed4bcaf3b828bbc8754b5ba00b43
adarsh3699 <adarsh3699@gmail.com>
2024-03-15 11:31:17 +0530

    get started

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home';
+import Home2 from './pages/Home2';
+import Home3 from './pages/Home3';
+import Home4 from './pages/Home4';
+import Page404 from './components/404/Page404';
+import BlogDetails from './components/Blog/BlogDetails';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import Layout2 from './components/Layout/Layout2';
+
+function App() {
+	const [data, setData] = useState({});
+	useEffect(() => {
+		Aos.init({ once: true });
+	}, []);
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const response = await fetch(
+					'https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae'
+				);
+				const data = await response.json();
+				if (data.success) {
+					setData(data);
+					console.log(data);
+				}
+			} catch (error) {
+				console.error('There was an error:', error);
+			}
+		})();
+	}, []);
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route path="home-v3" element={<Home3 />} />
+					<Route path="home-v4" element={<Home4 />} />
+					<Route path="*" element={<Page404 />} />
+					<Route path="blog/blog-details" element={<BlogDetails />} />
+				</Route>
+				<Route path="/home-v2" element={<Layout2 />}>
+					<Route index element={<Home2 />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
