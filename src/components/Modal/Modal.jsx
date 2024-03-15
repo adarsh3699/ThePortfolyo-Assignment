5120c3fb0e70c862a60dcf5e4514de159a6c9f74
adarsh3699 <adarsh3699@gmail.com>
2024-03-15 23:46:20 +0530

    Add click outside functionality to close modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,15 +1,33 @@
+import React, { useState, useEffect, useRef } from 'react';
 import './Modal.scss';
 import { Icon } from '@iconify/react';
 
 const Modal = ({ img, title, subTitle, liveurl, githuburl, modalClose }) => {
+	const modalRef = useRef();
 	const modalStyle = {
 		backgroundColor: 'rgba(0,0,0,0.8)',
 		display: 'block',
 	};
+
+	useEffect(function () {
+		document.addEventListener('click', handleClickOutside, true);
+
+		//component did un-mount
+		return function () {
+			document.removeEventListener('click', handleClickOutside, true);
+		};
+	}, []);
+
+	function handleClickOutside(e) {
+		if (modalRef.current && !modalRef.current.contains(e.target)) {
+			modalClose();
+		}
+	}
+
 	return (
 		<div className="modal show fade bd-example-modal-lg" style={modalStyle}>
 			<div className="modal-dialog modal-lg">
-				<div className="modal-content">
+				<div className="modal-content" ref={modalRef}>
 					<div className="modal-header">
 						<h4 className="modal-title">{title}</h4>
 						<button type="button" className="btn-close" onClick={modalClose}></button>
